551db49f535a7bc0076785690cdfc45b8cbca487
seanng <shonum@gmail.com>
2021-05-05 17:41:09 +0800

    initial commit: setup tailwind, firebase, eslint

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '@styles/globals.css'
+import { AuthProvider } from 'lib/auth'
+import Head from 'next/head'
+import 'tailwindcss/tailwind.css'
+import 'styles/globals.css'
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Gift Share Web</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        <meta name="description" content="Share gift purchaes with friends!" />
+      </Head>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </>
+  )
 }
 
-export default Application
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,36 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+const people = [
+  {
+    name: 'Calvin Hawkins',
+    email: 'calvin.hawkins@example.com',
+    image:
+      'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Kristen Ramos',
+    email: 'kristen.ramos@example.com',
+    image:
+      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Ted Fox',
+    email: 'ted.fox@example.com',
+    image:
+      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+]
 
-export default function Home() {
+export default function Example() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
+    <ul className="divide-y divide-gray-200">
+      {people.map((person) => (
+        <li key={person.email} className="py-4 flex">
+          <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
+          <div className="ml-3">
+            <p className="text-sm font-medium text-gray-900">{person.name}</p>
+            <p className="text-sm text-gray-500">{person.email}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
   )
 }
